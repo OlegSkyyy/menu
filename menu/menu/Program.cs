fd1c62f0befa397baa13954ccc9d473c5c1a853f
OlegSkyyy <e.chshegoleva@sibstrin.ru>
2023-04-27 00:06:56 +0700

    a

--- menu/menu/Program.cs
+++ menu/menu/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Dynamic;
 
 namespace menu // Note: actual namespace depends on the project name.
 {
@@ -9,11 +10,12 @@ namespace menu // Note: actual namespace depends on the project name.
             Console.WriteLine("Ввести A (1)\nВвести B (2)\nвыполнить операцию “+” (3)\nвыполнить операцию “-” (4)\nвыполнить операцию “*” (5)\nвыполнить операцию “/” (6)");
             string v;
             v = Console.ReadLine();
+            int a = 0;int b = 0;
             switch(v)
             {
                 case "1":
                     {
-
+                        a = Convert.ToInt32(Console.ReadLine());
                     }
                     break;
                 case "2":
