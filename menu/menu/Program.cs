b0ed1da5fe27adb59c127f64f9f9b5f6ff563f8b
OlegSkyyy <e.chshegoleva@sibstrin.ru>
2023-04-26 23:55:42 +0700

    lab

---
+++ menu/menu/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace menu // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввести A (1)\nВвести B (2)\nвыполнить операцию “+” (3)\nвыполнить операцию “-” (4)\nвыполнить операцию “*” (5)\nвыполнить операцию “/” (6)");
+            string v;
+            v = Console.ReadLine();
+            switch(v)
+            {
+                case "1":
+                    {
+
+                    }
+                    break;
+                case "2":
+                    {
+
+                    }
+                    break;
+                case "3":
+                    {
+
+                    }
+                    break;
+                case "4":
+                    {
+
+                    }
+                    break;
+                case "5":
+                    {
+
+                    }
+                    break;
+                case "6":
+                    {
+
+                    }
+                    break;
+            }
+        }
+    }
+}
